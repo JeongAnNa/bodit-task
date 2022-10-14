fee6f5f63b706e97206f317e15c545ffb155f8fd
JeongAnNa <rib040211@gmail.com>
2022-10-14 21:56:31 +0900

    [Modify] app.js

--- task/app.js
+++ task/app.js
@@ -1,8 +1,7 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const app = express();
 
 const route = {
   index: require('./routes/index'),
@@ -10,23 +9,21 @@ const route = {
   sum: require('./routes/sum'),
   primeNumber: require('./routes/primeNumber')
 }
-const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
+// route setup
 app.use('/', route.index);
 app.use('/equation', route.equation);
 app.use('/sum', route.sum)
 app.use('/prime-number', route.primeNumber)
 
-// catch 404 and forward to error handler
+// 404 Error 처리
 app.use(function(req, res, next) {
   next(createError(404));
 });
