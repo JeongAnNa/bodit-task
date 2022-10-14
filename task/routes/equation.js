7a9fe813bbed3cc1b062b9a3c9e6f6035be450c4
JeongAnNa <rib040211@gmail.com>
2022-10-14 21:50:19 +0900

    [Modify] 허근일 경우 status code, error message 수정

--- task/routes/equation.js
+++ task/routes/equation.js
@@ -22,7 +22,7 @@ router.post('/', [
     const d = b*b - (4*a*c)
     
     if (a == 0) { res.status(400).json({ message: "이차방정식이 아닙니다. a는 0이 될 수 없습니다." }); }
-    if (d < 0) { res.status(400).json({ message: "요청하신 방정식은 허근을 가지는 방정식입니다. 해당 API는 실근을 구하는 서비스를 제공하고 있습니다."})}
+    if (d < 0) { res.status(400).json({ message: "요청하신 방정식은 허근을 가지는 방정식입니다. 해당 API는 실근을 구하는 서비스만 제공하고 있습니다."})}
     else { res.json({x: equation(a, b, c)}) }
 });
 
